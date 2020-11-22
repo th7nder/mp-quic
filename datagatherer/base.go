c3315133c955d41ba276c5f2aa60aa770d478f69
Konrad Stepniak <vesphy@gmail.com>
2020-11-23 00:10:36 +0100

    Disable stat gathering

--- datagatherer/base.go
+++ datagatherer/base.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"sort"
 	"sync"
 	"time"
 
@@ -62,108 +61,110 @@ func (b *Base) OnPacketSent(pathID protocol.PathID, frames []wire.Frame) {
 	b.mut.Lock()
 	defer b.mut.Unlock()
 
-	now := time.Now()
-	for _, frame := range frames {
-		switch frame := frame.(type) {
-		case *wire.StreamFrame:
-			// given that offset + datalen are not overlapping
-			if _, ok := b.chunks[frame.StreamID]; !ok {
-				b.chunks[frame.StreamID] = make(map[protocol.ByteCount]*Chunk)
-			}
+	// now := time.Now()
+	// for _, frame := range frames {
+	// 	switch frame := frame.(type) {
+	// 	case *wire.StreamFrame:
+	// 		// given that offset + datalen are not overlapping
+	// 		if _, ok := b.chunks[frame.StreamID]; !ok {
+	// 			b.chunks[frame.StreamID] = make(map[protocol.ByteCount]*Chunk)
+	// 		}
 
-			b.chunks[frame.StreamID][frame.Offset] = &Chunk{
-				Sent: now,
-			}
-		default:
-		}
-	}
+	// 		b.chunks[frame.StreamID][frame.Offset] = &Chunk{
+	// 			Sent: now,
+	// 		}
+	// 	default:
+	// 	}
+	// }
 }
 
 func (b *Base) OnAckReceived(pathID protocol.PathID, frames []wire.Frame) {
 	b.mut.Lock()
 	defer b.mut.Unlock()
 
-	now := time.Now()
-	for _, frame := range frames {
-		switch frame := frame.(type) {
-		case *wire.StreamFrame:
-			if _, ok := b.chunks[frame.StreamID][frame.Offset]; !ok {
-				continue
-			}
-			difference := now.Sub(b.chunks[frame.StreamID][frame.Offset].Sent)
-			// streamID || offset || delay (ns)
-			b.rttWriter.WriteString(
-				fmt.Sprintf(
-					"%d,%d,%d,%d\n",
-					frame.StreamID,
-					frame.Offset,
-					b.chunks[frame.StreamID][frame.Offset].Sent.UnixNano(),
-					difference.Nanoseconds(),
-				),
-			)
-			b.chunks[frame.StreamID][frame.Offset].Acked = true
+	/*
+		now := time.Now()
+		for _, frame := range frames {
+			switch frame := frame.(type) {
+			case *wire.StreamFrame:
+				if _, ok := b.chunks[frame.StreamID][frame.Offset]; !ok {
+					continue
+				}
+				difference := now.Sub(b.chunks[frame.StreamID][frame.Offset].Sent)
+				// streamID || offset || delay (ns)
+				b.rttWriter.WriteString(
+					fmt.Sprintf(
+						"%d,%d,%d,%d\n",
+						frame.StreamID,
+						frame.Offset,
+						b.chunks[frame.StreamID][frame.Offset].Sent.UnixNano(),
+						difference.Nanoseconds(),
+					),
+				)
+				b.chunks[frame.StreamID][frame.Offset].Acked = true
 
-			// sort offsets first
-			keys := make([]protocol.ByteCount, len(b.chunks[frame.StreamID]))
-			i := 0
-			for k := range b.chunks[frame.StreamID] {
-				keys[i] = k
-				i++
-			}
-			sort.Slice(keys, func(i, j int) bool { return keys[i] < keys[j] })
+				// sort offsets first
+				keys := make([]protocol.ByteCount, len(b.chunks[frame.StreamID]))
+				i := 0
+				for k := range b.chunks[frame.StreamID] {
+					keys[i] = k
+					i++
+				}
+				sort.Slice(keys, func(i, j int) bool { return keys[i] < keys[j] })
 
-			var delayed bool
-			for _, offset := range keys {
-				chunk := b.chunks[frame.StreamID][offset]
+				var delayed bool
+				for _, offset := range keys {
+					chunk := b.chunks[frame.StreamID][offset]
 
-				if offset < frame.Offset {
-					delayed = true
-					// if this is delayed, it won't release everything later either
-					break
-				} else if offset > frame.Offset {
-					// chunk is not acked, therefore we need to wait.
-					// array is sorted, so everything above it will be blocked as well
-					if !chunk.Acked {
+					if offset < frame.Offset {
+						delayed = true
+						// if this is delayed, it won't release everything later either
 						break
+					} else if offset > frame.Offset {
+						// chunk is not acked, therefore we need to wait.
+						// array is sorted, so everything above it will be blocked as well
+						if !chunk.Acked {
+							break
+						}
+						// hooray, we're releasing chunks
+						diff := now.Sub(chunk.Sent)
+						b.delaysWriter.WriteString(
+							// stream | offset | sentTime (ns) | delay (ns)
+							fmt.Sprintf(
+								"%d,%d,%d,%d\n",
+								frame.StreamID,
+								offset,
+								chunk.Sent.UnixNano(),
+								diff.Nanoseconds(),
+							),
+						)
+
+						delete(b.chunks[frame.StreamID], offset)
 					}
-					// hooray, we're releasing chunks
+				}
+
+				// if nothing was blocking current chunk, just delete it and write to CSV as (ack_received - sent)
+				if !delayed {
+					chunk := b.chunks[frame.StreamID][frame.Offset]
 					diff := now.Sub(chunk.Sent)
 					b.delaysWriter.WriteString(
 						// stream | offset | sentTime (ns) | delay (ns)
 						fmt.Sprintf(
 							"%d,%d,%d,%d\n",
 							frame.StreamID,
-							offset,
+							frame.Offset,
 							chunk.Sent.UnixNano(),
 							diff.Nanoseconds(),
 						),
 					)
 
-					delete(b.chunks[frame.StreamID], offset)
+					delete(b.chunks[frame.StreamID], frame.Offset)
 				}
+
+			default:
 			}
-
-			// if nothing was blocking current chunk, just delete it and write to CSV as (ack_received - sent)
-			if !delayed {
-				chunk := b.chunks[frame.StreamID][frame.Offset]
-				diff := now.Sub(chunk.Sent)
-				b.delaysWriter.WriteString(
-					// stream | offset | sentTime (ns) | delay (ns)
-					fmt.Sprintf(
-						"%d,%d,%d,%d\n",
-						frame.StreamID,
-						frame.Offset,
-						chunk.Sent.UnixNano(),
-						diff.Nanoseconds(),
-					),
-				)
-
-				delete(b.chunks[frame.StreamID], frame.Offset)
-			}
-
-		default:
 		}
-	}
+	*/
 }
 
 func (b *Base) OnPathGatherSentStats(args *GatherSentStatsArgs) {
