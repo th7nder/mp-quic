ccc654772bcbb29a158ef379974ef38c4a46c85e
Konrad Stepniak <vesphy@gmail.com>
2021-01-10 23:41:14 +0100

    Remove buf

--- pconn_manager.go
+++ pconn_manager.go
@@ -84,10 +84,11 @@ func (pcm *pconnManager) setup(pconnArg net.PacketConn, listenAddr net.Addr) err
 func (pcm *pconnManager) listen(pconn net.PacketConn) {
 	var err error
 
-	if conn, ok := pconn.(interface{ SetReadBuffer(int) error }); ok {
-		utils.Infof("Changed received buffer size to %d", protocol.DesiredReceiveBufferSize)
-		conn.SetReadBuffer(protocol.DesiredReceiveBufferSize)
-	}
+	// Ustawienie bufora
+	// if conn, ok := pconn.(interface{ SetReadBuffer(int) error }); ok {
+	// 	utils.Infof("Changed received buffer size to %d", protocol.DesiredReceiveBufferSize)
+	// 	conn.SetReadBuffer(protocol.DesiredReceiveBufferSize)
+	// }
 listenLoop:
 	for {
 		var n int
